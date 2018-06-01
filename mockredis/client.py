4f1c648427f13c7a77261924b66aabe31a803a38
chechar99 <cesarrian@gmail.com>
2018-06-01 16:58:56 -0300

    Fix error in LREM

--- mockredis/client.py
+++ mockredis/client.py
@@ -723,7 +723,7 @@ class MockRedis(object):
         # Return the length of the list after the push operation
         return len(redis_list)
 
-    def lrem(self, key, value, count=0):
+    def lrem(self, key, count=0, value=''):
         """Emulate lrem."""
         value = self._encode(value)
         redis_list = self._get_list(key, 'LREM')
